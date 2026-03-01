3844c81460ac863c23578fabfa59ce335a2c05bc
Leigh Oliver <leighcharlesoliver@gmail.com>
2026-03-01 19:15:52 +1100

    WIP updated hash-fetching utilities.

--- utils/fetch-crosstool-versions.sh
+++ utils/fetch-crosstool-versions.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
+# Takes in a list of crosstool-NG version tags,
+# forms URLs for each of them,
+# then uses nix-prefetch-url and nix-hash to get the version hash.
+# the value 'N' is used to adjust parallelism
 
-for v in "15.2.0_20250920" "15.1.0_20250607";
-do
-    echo "\"${v}\" = {"
+version_list="crosstool-NG-tags.txt"
 
-      for chip in "xtensa" "riscv32";
+print_headers()
+{
+    echo "version,chip,arch,url,hash"
+}
+
+make_url()
+{
+    # tag is of the form 'esp-$version'
+    # but we want just the '$version' part.
+    v="${1#esp-}"
+    for chip in "xtensa" "riscv32";
+    do
+      for t in "x86_64-linux-gnu" "aarch64-apple-darwin" "aarch64-linux-gnu";
       do
-          echo "  \"${chip}\" = {"
+          url="https://github.com/espressif/crosstool-NG/releases/download/esp-${v}/${chip}-esp-elf-${v}-${t}.tar.xz"
 
-          for t in "x86_64-linux-gnu" "aarch64-apple-darwin" "aarch64-linux-gnu";
-          do
-              url="https://github.com/espressif/crosstool-NG/releases/download/esp-${v}/${chip}-esp-elf-${v}-${t}.tar.xz"
-              # hash the file then convert to SRI format
-              h=$(nix-prefetch-url --unpack --type sha256 "${url}" 2>/dev/null)
-              srihash=$(nix-hash --type sha256 --to-sri "${h}")
-              echo "    \"${t}\" = \"${srihash}\";"
-          done
-
-          echo "  };"
+          # hash the file then convert to SRI format
+          h=$(nix-prefetch-url --unpack --type sha256 "${url}" 2>/dev/null)
+          srihash=$(nix-hash --type sha256 --to-sri "${h}" 2>/dev/null)
+            
+          if [[ "$?" -eq 0 ]];
+          then
+              echo "${v},${chip},${t},${url},${srihash}"
+          else
+              echo "${v},${chip},${t},${url},XXXXXXXXXXXXXXXX"
+          fi
       done
+    done
+}
 
-    echo "};"
-done
+export -f make_url
+print_headers
+cat "${version_list}" | xargs -P1 -I{} bash -c 'make_url "{}"'
